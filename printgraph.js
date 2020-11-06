75bdc0352a86995bec52d90b58be1baa8853c7c5
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-06 09:40:51 -0600

    Update printgraph.js

--- printgraph.js
+++ printgraph.js
@@ -1,7 +1,7 @@
 class Graph{
  constructor(nbresommets){
-   this.nbresommets = nbresommets;
-   this.listadj = new Map();
+  this.nbresommets = nbresommets;
+  this.listadj = new Map();
  }
  addsommet(v){
   this.listadj.set(v,[]);
@@ -13,43 +13,47 @@ class Graph{
  printgraph(){
   var getkeys = this.listadj.keys();
   for(var i of getkeys){
-     var conc = " ";
-     var listsommet = this.listadj.get(i);
-     for(var j of listsommet)
+      var conc = " ";
+      var listsommet = this.listadj.get(i);
+      for(var j of listsommet)
         conc = conc + j;
-   console.log(i + " " + " ->" + " " + conc);
+  console.log(i + " " + " -> " + " " + conc);
   }
- }
-}//end class graph
-var tab = ['A','B','C','D','E','D','F','G','H','I','J','K','L','M','N','O'];
-var test = new Graph(15);
-for (var i =0; i < tab.length; i++)
-      test.addsommet(tab[i]);
-      test.addchemin('A','B');
-      test.addchemin('A','C');
-      test.addchemin('A','L');
-      test.addchemin('B','M');
-      test.addchemin('B','D');
-      test.addchemin('C','E');
-      test.addchemin('D','G');
-      test.addchemin('D','E');
-      test.addchemin('E','H');
-      test.addchemin('F','H');
-      test.addchemin('F','J');
-      test.addchemin('F','K');
-      test.addchemin('F','L');
-      test.addchemin('G','M');
-      test.addchemin('G','I');
-      test.addchemin('H','I');
-      test.addchemin('H','N');
-      test.addchemin('J','O');
-      test.addchemin('J','K');
-      test.addchemin('K','O');
-      test.addchemin('K','L');
-      test.addchemin('M','N');
-      test.addchemin('N','O');
-      console.log("OUR GRAPH");
-      test.printgraph();
+ } // end printgraph
+}// end class Graph
+var tab = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'];
+var test = new Graph(20);
+for(var i = 0; i < tab.length; i++)
+    test.addsommet(tab[i]);
+    test.addchemin('A','B');
+    test.addchemin('A','C');
+    test.addchemin('A','D');
+    test.addchemin('B','E');
+    test.addchemin('C','E');
+    test.addchemin('C','F');
+    test.addchemin('D','H');
+    test.addchemin('E','J');
+    test.addchemin('F','G');
+    test.addchemin('F','K');
+    test.addchemin('G','H');
+    test.addchemin('H','K');
+    test.addchemin('H','L');
+    test.addchemin('H','I');
+    test.addchemin('I','L');
+    test.addchemin('J','K');
+    test.addchemin('K','M');
+    test.addchemin('L','O');
+    test.addchemin('M','P');
+    test.addchemin('N','O');
+    test.addchemin('N','P');
+    test.addchemin('P','T');
+    test.addchemin('Q','S');
+    test.addchemin('Q','R');
+    test.addchemin('R','S');
+    test.addchemin('T','Q');
+    test.printgraph();
+
+
 
 
 
